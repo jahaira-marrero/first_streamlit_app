e06ac75a42641fa1e14ba5ad1c79183446275750
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-20 11:56:50 -0400

    Update streamlit_app.py
    set the fruit list index to fruit and added a multiselect for users to pick fruits from the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 streamlit.title("My Mom\'s New Healthy Diner")
 
@@ -12,4 +13,5 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header("🍌🍓 Build Your Own Fruit Smoothie 🥝🍇")
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
