3d3e0c8e542bdea27e133a5716e3ccb9b927a3c8
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-21 14:33:11 -0400

    Update streamlit_app.py
    added user input for requesting fruit information, formatted the get request to add  the user's input

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
@@ -24,5 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 #streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered, fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice")
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
