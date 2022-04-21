a6abf4c2e6ba35c97ce6baeba2ebc439f0347990
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-21 14:23:25 -0400

    Update streamlit_app.py
    debugged pandas to pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
