d01f5ed5159691bb9f89fe2599c523a948091762
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-22 10:05:39 -0400

    Update streamlit_app.py
    created a function that allows user to add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,14 +51,23 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-
+def  insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('"+{new_fruit}+"')")
+    return "Thanks for adding" + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-
-def show_add(add_my_fruit):
-  if len(add_my_fruit)>= 1:
-    return streamlit.write(f'Thanks for adding {add_my_fruit}.')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
   
-show_add(add_my_fruit)
+    
+
+#def show_add(add_my_fruit):
+  #if len(add_my_fruit)>= 1:
+    #return streamlit.write(f'Thanks for adding {add_my_fruit}.')
+  
+#show_add(add_my_fruit)
 streamlit.stop()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
