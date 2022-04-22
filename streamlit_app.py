f178dee84bca1be188ae4ab4d0bb8480aacfecfc
Jahaira Marrero <70103241+jahaira-marrero@users.noreply.github.com>
2022-04-22 10:24:54 -0400

    Update streamlit_app.py
    closed the snowflake connection after each button, changed the header of the Fruit List section and the text of the button to 'Get Fruit List'

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,15 +40,16 @@ except URLError as e:
     streamlit.error()
 
 #Getting the Fruit Load List
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 def  insert_row_snowflake(new_fruit):
@@ -60,14 +61,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
-  
-    
 
-#def show_add(add_my_fruit):
-  #if len(add_my_fruit)>= 1:
-    #return streamlit.write(f'Thanks for adding {add_my_fruit}.')
-  
-#show_add(add_my_fruit)
 streamlit.stop()
 
